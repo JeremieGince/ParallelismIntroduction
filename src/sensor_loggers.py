6aae66b805e965ce43dc64a54ccdf8fb4b3019db
Gabriel Genest <9.genest.gabriel@gmail.com>
2022-03-03 11:35:55 -0500

    Update sensor_loggers.py

--- src/sensor_loggers.py
+++ src/sensor_loggers.py
@@ -10,75 +10,73 @@ from .sensors.sensor import Sensor
 
 
 class SensorLogger(Thread):
-	sensors_data_folder = "./data/sensors_data/"
-	sensors_data_filename = "datalog.csv"
+    sensors_data_folder = "./data/sensors_data/"
+    sensors_data_filename = "datalog.csv"
 
-	def __init__(self, sensor: Sensor, lock: Union[mp.RLock, mp.Lock]):
-		super().__init__()
-		self._sensor = sensor
-		self._date = None
-		self._stop_event = Event()
-		self.lock = lock
-		# counter
-		self.count = 0
+    def __init__(self, sensor: Sensor, lock: Union[mp.RLock, mp.Lock]):
+        super().__init__()
+        self._sensor = sensor
+        self._date = None
+        self._stop_event = Event()
+        self.lock = lock
+        # counter
+        self.count = 0
 
-		# stats
-		self.min = np.inf
-		self.mean = 0
-		self.max = -np.inf
+        # stats
+        self.min = np.inf
+        self.mean = 0
+        self.max = -np.inf
 
-	@staticmethod
-	def get_data_filename():
-		return f"{SensorLogger.sensors_data_folder}/{SensorLogger.sensors_data_filename}"
+    @staticmethod
+    def get_data_filename():
+        return f"{SensorLogger.sensors_data_folder}/{SensorLogger.sensors_data_filename}"
 
-	def set_date(self, date):
-		self._date = date
-		self._sensor.set_date(date)
+    def set_date(self, date):
+        self._date = date
+        self._sensor.set_date(date)
 
-	def stop(self):
-		self._stop_event.set()
+    def stop(self):
+        self._stop_event.set()
 
-	def create_load_log_file(self):
-		with self.lock:
-			if os.path.exists(SensorLogger.get_data_filename()):
-				df = pd.read_csv(SensorLogger.get_data_filename(), index_col=['Date'])
-			else:
-				df = pd.DataFrame(columns=["Date", *self._sensor.columns_names])
+    def create_load_log_file(self):
+        with self.lock:
+            if os.path.exists(SensorLogger.get_data_filename()):
+                df = pd.read_csv(SensorLogger.get_data_filename())
+            else:
+                fill = [self._date] + [np.nan] * len(self._sensor.columns_names)
+                df = pd.DataFrame([fill], columns=["Date", *self._sensor.columns_names])
 
-			for col in ["Date", *self._sensor.columns_names]:
-				if col not in df.columns:
-					df.insert(len(df.columns), col, np.NaN)
+            for col in self._sensor.columns_names:
+                if col not in df.columns:
+                    print("Ici")
+                    df.insert(len(df.columns), col, np.NaN)
+            df.to_csv(SensorLogger.get_data_filename(), index=False)
 
-			df['Date'] = pd.to_datetime(df['Date'])
-			df.set_index('Date', inplace=True)
-			df.to_csv(SensorLogger.get_data_filename())
+    def update_log_file(self):
+        with self.lock:
+            df = pd.read_csv(SensorLogger.get_data_filename(), index_col="Date")
+            date = self._date
+            df.loc[date, self._sensor.columns_names] = [self.min, self.max, self.mean]
+            df.to_csv(SensorLogger.get_data_filename())
 
-	def update_log_file(self):
-		with self.lock:
-			df = pd.read_csv(SensorLogger.get_data_filename(), index_col=['Date'])
-			date = str(pd.to_datetime(self._date).date())
-			print(date)
-			df.loc[date, self._sensor.columns_names] = [self.min, self.max, self.mean]
-			df.to_csv(SensorLogger.get_data_filename())
+    def run(self):
+        """
+        TODO: read the value of the sensor as much as it can and every time read is called, save the current stats
+        TODO: on the shared file .csv
 
-	def run(self):
-		"""
-		TODO: read the value of the sensor as much as it can and every time read is called, save the current stats
-		TODO: on the shared file .csv
+        tips: use lock to make sure you don't overwrite over other thread (sensor).
 
-		tips: use lock to make sure you don't overwrite over other thread (sensor).
+        :return: None
+        """
+        self.create_load_log_file()
+        while not self._stop_event.is_set():
+            val = self._sensor.read()
+            self.min = min(self.min, val)
+            self.max = max(self.max, val)
+            self.mean = (self.count * self.mean + val) / (self.count + 1)
+            self.count += 1
+            self.update_log_file()
 
-		:return: None
-		"""
-		self.create_load_log_file()
-		while not self._stop_event.is_set():
-			val = self._sensor.read()
-			self.min = min(self.min, val)
-			self.max = max(self.max, val)
-			self.mean = (self.count * self.mean + val) / (self.count + 1)
-			self.count += 1
-			self.update_log_file()
-	
-	def join(self, timeout=None):
-		self._stop_event.set()
-		super(SensorLogger, self).join(timeout)
+    def join(self, timeout=None):
+        self._stop_event.set()
+        super(SensorLogger, self).join(timeout)
