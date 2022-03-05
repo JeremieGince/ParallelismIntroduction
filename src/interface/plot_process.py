77531d3aad1e60d0ca2e36bfc2d706d7cec920b2
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-03-05 15:23:49 -0500

    Update plot_process.py

--- src/interface/plot_process.py
+++ src/interface/plot_process.py
@@ -21,6 +21,7 @@ class PlotProcess(mp.Process):
 		self.update_dt = update_dt
 		self.sensor_to_ax = {}
 		self.sensor_to_lines = {}
+		self.X = None
 
 	def run(self):
 		while not os.path.exists(self._log_file) and not self._close_event.is_set():
@@ -48,14 +49,14 @@ class PlotProcess(mp.Process):
 		with self._lock:
 			df = pd.read_csv(self._log_file, index_col="Date")
 
-		X = df.index.tolist()
-		if self._date not in X:
-			X.append(self._date)
+		self.X = df.index.tolist()
+		if self._date not in self.X:
+			self.X.append(self._date)
 		for i, sensor in enumerate(self._sensors):
 			axes[i].set_title(sensor.name)
 			axes[i].set_ylabel(sensor.units)
 			low, high, avg = df[sensor.columns_names].to_numpy().transpose()
-			if len(low) < len(X):
+			if len(low) < len(self.X):
 				low = list(low) + [0, ]
 				high = list(high) + [0, ]
 				avg = list(avg) + [0, ]
@@ -63,7 +64,7 @@ class PlotProcess(mp.Process):
 			high_line, = axes[i].plot(high, label='high', marker='o')
 			avg_line, = axes[i].plot(avg, label='avg', marker='o')
 			if (i+1) > ncols*(nrows-1):
-				axes[i].set_xticks(range(len(X)), X, rotation=45)
+				axes[i].set_xticks(range(len(self.X)), self.X, rotation=45)
 			else:
 				axes[i].tick_params(
 					axis='x',  # changes apply to the x-axis
@@ -84,7 +85,7 @@ class PlotProcess(mp.Process):
 			low, high, avg = df.loc[self._date, sensor.columns_names]
 			for line_name, new_y in zip(['low', 'high', 'avg'], [low, high, avg]):
 				y = self.sensor_to_lines[sensor][line_name].get_ydata()
-				y[-1] = new_y
+				y[self.X.index(self._date)] = new_y
 				self.sensor_to_lines[sensor][line_name].set_ydata(y)
 			self.sensor_to_ax[sensor].relim()
 			self.sensor_to_ax[sensor].autoscale_view()
