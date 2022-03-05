1afca28b5824fcb6eb81927321facc82dc5b1709
Gabriel Genest <9.genest.gabriel@gmail.com>
2022-03-05 15:32:57 -0500

    INDENTATION

--- src/interface/plot_process.py
+++ src/interface/plot_process.py
@@ -8,101 +8,96 @@ import numpy as np
 
 
 class PlotProcess(mp.Process):
-	"""
-	TODO
-	"""
-	def __init__(self, sensors, lock, log_file, date, update_dt: float = 1.0):
-		super(PlotProcess, self).__init__()
-		self._sensors = sensors
-		self._lock = lock
-		self._close_event = mp.Event()
-		self._log_file = log_file
-		self._date = date
-		self.update_dt = update_dt
-		self.sensor_to_ax = {}
-		self.sensor_to_lines = {}
-		self.X = None
+    """
+    TODO
+    """
 
-	def run(self):
-		can_plot = False
-		while not can_plot and not self._close_event.is_set():
-			if os.path.exists(self._log_file):
-				try:
-					pd.read_csv(self._log_file, index_col="Date")
-					can_plot = True
-				except:
-					time.sleep(self.update_dt)
+    def __init__(self, sensors, lock, log_file, date, update_dt: float = 1.0):
+        super(PlotProcess, self).__init__()
+        self._sensors = sensors
+        self._lock = lock
+        self._close_event = mp.Event()
+        self._log_file = log_file
+        self._date = date
+        self.update_dt = update_dt
+        self.sensor_to_ax = {}
+        self.sensor_to_lines = {}
+        self.X = None
 
-		self.create_plot()
-		plt.legend()
-		plt.pause(self.update_dt)
-		while not self._close_event.is_set():
-			self.update_plot()
-			plt.pause(self.update_dt)
+    def run(self):
+        can_plot = False
+        while not can_plot and not self._close_event.is_set():
+            if os.path.exists(self._log_file):
+                try:
+                    pd.read_csv(self._log_file, index_col="Date")
+                    can_plot = True
+                except:
+                    time.sleep(self.update_dt)
 
-	def create_plot(self):
-		self.sensor_to_ax.clear()
-		self.sensor_to_lines.clear()
+        self.create_plot()
+        plt.legend()
+        plt.pause(self.update_dt)
+        while not self._close_event.is_set():
+            self.update_plot()
+            plt.pause(self.update_dt)
 
-		ncols = int(np.sqrt(len(self._sensors)))
-		nrows = int(len(self._sensors) / ncols)
-		figure, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 8))
-		figure.canvas.manager.full_screen_toggle()
-		if isinstance(axes, plt.Axes):
-			axes = np.asarray([axes])
-		else:
-			axes = axes.flatten()
-		with self._lock:
-			df = pd.read_csv(self._log_file, index_col="Date")
+    def create_plot(self):
+        self.sensor_to_ax.clear()
+        self.sensor_to_lines.clear()
 
-		self.X = df.index.tolist()
-		if self._date not in self.X:
-			self.X.append(self._date)
-		for i, sensor in enumerate(self._sensors):
-			axes[i].set_title(sensor.name)
-			axes[i].set_ylabel(sensor.units)
-			low, high, avg = df[sensor.columns_names].to_numpy().transpose()
-			if len(low) < len(self.X):
-				low = list(low) + [0, ]
-				high = list(high) + [0, ]
-				avg = list(avg) + [0, ]
-			low_line, = axes[i].plot(low, label='low', marker='o')
-			high_line, = axes[i].plot(high, label='high', marker='o')
-			avg_line, = axes[i].plot(avg, label='avg', marker='o')
-			if (i+1) > ncols*(nrows-1):
-				axes[i].set_xticks(range(len(self.X)), self.X, rotation=45)
-			else:
-				axes[i].tick_params(
-					axis='x',  # changes apply to the x-axis
-					which='both',  # both major and minor ticks are affected
-					bottom=False,  # ticks along the bottom edge are off
-					top=False,  # ticks along the top edge are off
-					labelbottom=False,  # labels along the bottom edge are off
-				)
-			self.sensor_to_ax[sensor] = axes[i]
-			self.sensor_to_lines[sensor] = dict(low=low_line, high=high_line, avg=avg_line)
-		for i in range(len(self._sensors), len(axes)):
-			axes[i].axis('off')
+        ncols = int(np.sqrt(len(self._sensors)))
+        nrows = int(len(self._sensors) / ncols)
+        figure, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 8))
+        figure.canvas.manager.full_screen_toggle()
+        if isinstance(axes, plt.Axes):
+            axes = np.asarray([axes])
+        else:
+            axes = axes.flatten()
+        with self._lock:
+            df = pd.read_csv(self._log_file, index_col="Date")
 
-	def update_plot(self):
-		with self._lock:
-			df = pd.read_csv(self._log_file, index_col="Date")
-		for i, sensor in enumerate(self._sensors):
-			low, high, avg = df.loc[self._date, sensor.columns_names]
-			for line_name, new_y in zip(['low', 'high', 'avg'], [low, high, avg]):
-				y = self.sensor_to_lines[sensor][line_name].get_ydata()
-				y[self.X.index(self._date)] = new_y
-				self.sensor_to_lines[sensor][line_name].set_ydata(y)
-			self.sensor_to_ax[sensor].relim()
-			self.sensor_to_ax[sensor].autoscale_view()
-		plt.draw()
+        self.X = df.index.tolist()
+        if self._date not in self.X:
+            self.X.append(self._date)
+        for i, sensor in enumerate(self._sensors):
+            axes[i].set_title(sensor.name)
+            axes[i].set_ylabel(sensor.units)
+            low, high, avg = df[sensor.columns_names].to_numpy().transpose()
+            if len(low) < len(self.X):
+                low = list(low) + [0, ]
+                high = list(high) + [0, ]
+                avg = list(avg) + [0, ]
+            low_line, = axes[i].plot(low, label='low', marker='o')
+            high_line, = axes[i].plot(high, label='high', marker='o')
+            avg_line, = axes[i].plot(avg, label='avg', marker='o')
+            if (i + 1) > ncols * (nrows - 1):
+                axes[i].set_xticks(range(len(self.X)), self.X, rotation=45)
+            else:
+                axes[i].tick_params(
+                    axis='x',  # changes apply to the x-axis
+                    which='both',  # both major and minor ticks are affected
+                    bottom=False,  # ticks along the bottom edge are off
+                    top=False,  # ticks along the top edge are off
+                    labelbottom=False,  # labels along the bottom edge are off
+                )
+            self.sensor_to_ax[sensor] = axes[i]
+            self.sensor_to_lines[sensor] = dict(low=low_line, high=high_line, avg=avg_line)
+        for i in range(len(self._sensors), len(axes)):
+            axes[i].axis('off')
 
-	def join(self, *args, **kwargs):
-		self._close_event.set()
-		super(PlotProcess, self).join(*args, **kwargs)
+    def update_plot(self):
+        with self._lock:
+            df = pd.read_csv(self._log_file, index_col="Date")
+        for i, sensor in enumerate(self._sensors):
+            low, high, avg = df.loc[self._date, sensor.columns_names]
+            for line_name, new_y in zip(['low', 'high', 'avg'], [low, high, avg]):
+                y = self.sensor_to_lines[sensor][line_name].get_ydata()
+                y[self.X.index(self._date)] = new_y
+                self.sensor_to_lines[sensor][line_name].set_ydata(y)
+            self.sensor_to_ax[sensor].relim()
+            self.sensor_to_ax[sensor].autoscale_view()
+        plt.draw()
 
-
-
-
-
-
+    def join(self, *args, **kwargs):
+        self._close_event.set()
+        super(PlotProcess, self).join(*args, **kwargs)
